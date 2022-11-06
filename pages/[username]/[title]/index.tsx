ab0e3f7d64f4d8e77384332f044043c703fd787b
dubisdev <dubisdev@gmail.com>
2022-11-06 20:24:20 +0100

    chore: migrate deprecated method

--- pages/[username]/[title]/index.tsx
+++ pages/[username]/[title]/index.tsx
@@ -1,5 +1,8 @@
-import { withPageAuth } from "@supabase/auth-helpers-nextjs";
+import {
+  createServerSupabaseClient,
+} from "@supabase/auth-helpers-nextjs";
 import { encode } from "js-base64";
+import { GetServerSidePropsContext } from "next";
 import { getSongByKey, getUsernameId } from "../../../utils/database";
 
 export default function empty() {
@@ -11,29 +14,25 @@ const redirectTo = (path: string) => ({
   props: {},
 });
 
-export const getServerSideProps = withPageAuth({
-  async getServerSideProps(ctx, supabase) {
-    const { username, title } = ctx.params || {};
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const supabase = createServerSupabaseClient(ctx);
+  const { username, title } = ctx.params || {};
 
-    const { data, error } = await getUsernameId(supabase, username as string);
-    if (error || data.length === 0) return redirectTo("/");
-    const userId = data[0].id;
+  const { data, error } = await getUsernameId(supabase, username as string);
+  if (error || data.length === 0) return redirectTo("/");
+  const userId = data[0].id;
 
-    const songKey = { userId, songName: title as string };
+  const songKey = { userId, songName: title as string };
 
-    const { data: songData, error: err } = await getSongByKey(
-      supabase,
-      songKey
-    );
+  const { data: songData, error: err } = await getSongByKey(supabase, songKey);
 
-    if (err || songData?.length === 0) return redirectTo("/");
+  if (err || songData?.length === 0) return redirectTo("/");
 
-    const { title: songTitle, content: songContent } = songData[0];
+  const { title: songTitle, content: songContent } = songData[0];
 
-    // TODO - Instead of redirecting we can just render the page with the song data and allow the user to edit it and save it
-    // TODO - Add a button "Create New Song" that will redirect to the editor page with a new song
-    // TODO - Add a button "Delete Song" that will delete the song from the database
-    // TODO - If the user is not the owner of the song, they should not be able to edit it, just see it (maybe add a button to fork it)
-    return redirectTo(`/${encode(songTitle)}|${encode(songContent)}`);
-  },
-});
+  // TODO - Instead of redirecting we can just render the page with the song data and allow the user to edit it and save it
+  // TODO - Add a button "Create New Song" that will redirect to the editor page with a new song
+  // TODO - Add a button "Delete Song" that will delete the song from the database
+  // TODO - If the user is not the owner of the song, they should not be able to edit it, just see it (maybe add a button to fork it)
+  return redirectTo(`/${encode(songTitle)}|${encode(songContent)}`);
+};
