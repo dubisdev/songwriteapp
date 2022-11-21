7bbb6076b252ef6e7cd9980f1133194e686d818c
dubisdev <dubisdev@gmail.com>
2022-11-21 12:17:08 +0100

    fix: return to next12

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,15 +3,17 @@ import Link from "next/link";
 const Nav = () => {
   return (
     <nav className="flex w-full py-4 px-8 m-auto justify-between mb-2">
-      <Link href="/" className="inline-flex items-center">
-        <img
-          src="/logo.png"
-          className="mr-3 h-12 sm:h-12 inline"
-          alt="SongWrite.app Logo"
-        />
-        <h1 id="title" className="inline text-3xl font-bold underline">
-          SongWrite.app
-        </h1>
+      <Link href="/">
+        <a className="inline-flex items-center">
+          <img
+            src="/logo.png"
+            className="mr-3 h-12 sm:h-12 inline"
+            alt="SongWrite.app Logo"
+          />
+          <h1 id="title" className="inline text-3xl font-bold underline">
+            SongWrite.app
+          </h1>
+        </a>
       </Link>
     </nav>
   );
